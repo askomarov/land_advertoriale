cc1186afa4141bdf61060833c9228d27b49b57a9
askomarov <askomarov13@mail.ru>
2022-11-21 18:41:03 +0100

    fix: парвки после ревью

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+import easySetup from "easy-toggler";
+import { initTabs } from "./modules/init-tabs.js";
+import { initAccordion } from "./initAccordion.js";
+import { initHeader } from "./modules/initHeader.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // EasyToggler: https://github.com/rah-emil/easy-toggler
+  easySetup({
+    // html attrs
+    toggle: "easy-toggle",
+    add: "easy-add",
+    remove: "easy-remove",
+    class: "easy-class",
+    rcoe: "easy-rcoe",
+    parallel: "easy-parallel",
+    self: "easy-self",
+    selfRcoe: "easy-self-rcoe",
+
+    // Hooks
+    onToggle($el) {},
+    onAdd($el) {},
+    onRemove($el) {},
+    onRcoe($el) {},
+  });
+
+  window.addEventListener("load", () => {
+    console.log("hello!");
+    initAccordion();
+    initTabs();
+    initHeader();
+  });
+});
---
+++ source/js/modules/initHeader.js
@@ -0,0 +1,50 @@
+const headerMenu = document.querySelector(".header-menu");
+const menuButton = document.querySelector(".menu-button");
+
+const headerLinks = document.querySelectorAll(".header-menu__link");
+
+const showMenu = () => {
+  menuButton.setAttribute("aria-expanded", "true");
+  menuButton.setAttribute("aria-label", "close menu");
+  headerMenu.classList.remove("header-menu--closed");
+  headerMenu.classList.add("header-menu--opened");
+};
+
+const closeMenu = () => {
+  menuButton.setAttribute("aria-expanded", "false");
+  menuButton.setAttribute("aria-label", "open menu");
+  headerMenu.classList.add("header-menu--closed");
+  headerMenu.classList.remove("header-menu--opened");
+};
+
+// //////////////// КОНЕЦ меню бургер
+const initHeader = () => {
+  headerMenu.classList.remove("header-menu--no-js");
+  headerMenu.classList.add("header-menu--closed");
+
+  menuButton.addEventListener("click", () => {
+    if (headerMenu.classList.contains("header-menu--closed")) {
+      showMenu();
+    } else {
+      closeMenu();
+    }
+  });
+
+  headerLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      console.log("click");
+      if (headerMenu.classList.contains("header-menu--opened")) {
+        closeMenu();
+      }
+      let target = document.querySelector("#" + link.href.split("#")[1]);
+      console.log(target.offsetTop);
+
+      window.scroll({
+        behavior: "smooth",
+        left: 0,
+        top: target.offsetTop,
+      });
+    });
+  });
+};
+export { initHeader };
